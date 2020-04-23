6f9d074e86da4f17333a3060262589e81fd37276
Egor Pershin <hickar@icloud.com>
2020-04-24 00:49:09 +0300

    Exercise 1.2.2 done right

--- ex_1.2.2/ex_1.2.2.cpp
+++ ex_1.2.2/ex_1.2.2.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
-
-using namespace std;
 
 /* Создать объект, который обрабатывает массив целых чисел не более 10 элементов.
  * Количество элементов определяется в момент конструирования объекта.
@@ -25,16 +22,34 @@ using namespace std;
  *
  *  Выходные данные:
  *      Первая строка: N = количество элементов.
- *      Вторая строка: (исходный порядок следования элементов).
- *      Третья строка: (обратный порядок следования элементов).
+ *      Вторая строка (исходный порядок следования элементов):
+ *      целочисленные значения элементов массива, каждое значение занимает 5 позиций.
+ *      Третья строка (обратный порядок следования элементов):
+ *      целочисленные значения элементов массива, каждое значение занимает 5 позиций.
  */
 
-vector<int> split(const string &str, const char delimiter) {
-    vector<int> tokens;
-    string token;
-    istringstream tokenStream(str);
+bool isNumber(std::string number) {
+    bool isNumber = false;
+    try {
+        stoi(number);
+        isNumber = true;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return isNumber;
+}
+
+int* split(const std::string &str, int arraySize, const char delimiter) {
+    int* tokens = new int[arraySize];
+    int partIndex = 0;
+    std::string token;
+    std::istringstream tokenStream(str);
     while(getline(tokenStream, token, delimiter)) {
-        tokens.push_back(stoi(token));
+        tokens[partIndex] = stoi(token);
+        partIndex++;
+    }
+    if (partIndex != arraySize) {
+        throw "Не совпадает ранее указанный размер массива с его фактическим";
     }
     return tokens;
 }
@@ -42,49 +57,61 @@ vector<int> split(const string &str, const char delimiter) {
 class ArrayReversingObject {
 public:
     ArrayReversingObject() {
-        string input;
-        getline(cin, input);
-        arrSize = stoi(input);
+        std::string input;
+        getline(std::cin, input);
+        arraySize = stoi(input);
 
-        getline(cin, input);
-        arr = split(input, ' ');
-        if (arr.size() != arrSize) {
-            throw "Фактическое количество элементов в массиве отличается от указаного ранее";
-        }
+        getline(std::cin, input);
+        array = split(input, arraySize, ' ');
+
+        printArray();
+        reverseArray();
         printArray();
     }
 
     void reverseArray() {
         int temp,
             start = 0,
-            end = arrSize - 1;
+            end = arraySize - 1;
         while (start < end) {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
+            temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
             start++;
             end--;
         }
-        printArray();
     }
 
 private:
-    int arrSize;
-    vector<int> arr;
+    int* array;
+    int arraySize;
 
     void printArray() {
-        for (const int element : arr) {
-            printf("%i ", element);
+        for (int i = 0; i < arraySize; i++) {
+            int spacesCount = 5 - digitsCount(array[i]);
+            printf("%i", array[i]);
+            for (int i = 0; i < spacesCount; i++) {
+                printf(" ");
+            }
         }
         printf("\n");
+    }
+
+    int digitsCount(int num) {
+        int digitsCount = 0;
+        while (true) {
+            num /= 10;
+            digitsCount++;
+            if (num == 0) break;
+        }
+        return digitsCount;
     }
 };
 
 int main() {
     try {
         ArrayReversingObject arrayReverser;
-        arrayReverser.reverseArray();
     } catch (const char *msg) {
-        cerr << msg << endl;
+        std::cerr << msg << std::endl;
     }
 }
